a587b8d2b4c2dc3f3840e5226781dc267e2283eb
Bran Quanne <branmq@gmail.com>
2025-09-10 10:34:44 +0200

    question 11

--- dup.c
+++ dup.c
@@ -1,25 +1,44 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-    int fd = open("fil", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd == -1) {
-        perror("open");
+char** parse_line(char* buffer) {
+    int size = 8;
+    char** args = malloc(size * sizeof(char*));
+    char* token = strtok(buffer, " \t\n");
+
+    int i = 0;
+    while (token != NULL) {
+        if (i >= size - 1) {
+            size *= 2;
+            args = realloc(args, size * sizeof(char*));
+        }
+
+        args[i++] = strdup(token);
+        token = strtok(NULL, " \t\n");
+    }
+    args[i] = NULL;
+    return args;
+}
+
+int main(int argc, char* argv[]) {
+    char line[256];
+    printf("Write a command: ");
+    if (fgets(line, sizeof(line), stdin) == NULL) {
+        perror("fgets");
         exit(1);
     }
 
-    if (dup2(fd, STDOUT_FILENO) == -1) {
-        perror("dup2");
-        close(fd);
-        exit(1);
+    char** args = parse_line(line);
+    for (int i = 0; args[i] != NULL; i++) {
+        printf("args[%d]: %s\n", i, args[i]);
+        free(args[i]);
     }
+    free(args);
 
-    printf("Ey wallah!");
-
-    close(fd);
     exit(0);
 }
